7fc5df89f05031febeee0d4f01cd0439517323fa
AShunevich <48925983+AShunevich@users.noreply.github.com>
2020-12-17 22:25:22 +0200

    - implemented basic stopwatch functions

--- app/src/main/java/com/ashunevich/multitimer/StopwatchFragment.kt
+++ app/src/main/java/com/ashunevich/multitimer/StopwatchFragment.kt
@@ -1,20 +1,28 @@
 package com.ashunevich.multitimer
 
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ashunevich.multitimer.databinding.StopwatchFragBinding
-import com.ashunevich.multitimer.databinding.TimerFragBinding
 
 class StopwatchFragment : Fragment() {
+
+    enum class ChronometerState {
+        Stopped, Paused, Running
+    }
 
     private var _binding: StopwatchFragBinding? = null
 
     private val binding get() = _binding
+    private val format: String = "00:%s"
+    private var elapse:Int = 0
+    private var timerState = ChronometerState.Stopped
 
-    fun newInstance():StopwatchFragment{
+
+    fun newInstance(): StopwatchFragment {
         return StopwatchFragment()
     }
 
@@ -22,7 +30,39 @@ class StopwatchFragment : Fragment() {
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         _binding = StopwatchFragBinding.inflate(inflater, container, false)
+        binding?.totalChronometre?.format =format
+
+        initButton()
         return binding?.root
     }
 
+    fun initButton() {
+        binding?.fabStartPause?.setOnClickListener {
+            if (timerState == ChronometerState.Stopped) {
+                timerState = ChronometerState.Running
+                binding?.totalChronometre?.base = SystemClock.elapsedRealtime()
+                binding?.totalChronometre?.start()
+                //binding?.currentInterval?.start()
+                binding?.fabStartPause?.setImageResource(R.drawable.ic_pause)
+            }
+            else if (timerState ==ChronometerState.Running){
+                binding?.totalChronometre?.stop()
+               // binding?.currentInterval?.stop()
+                timerState = ChronometerState.Paused
+                elapse = (SystemClock.elapsedRealtime() - binding?.totalChronometre?.base!!).toInt();
+                binding?.fabStartPause?.setImageResource(R.drawable.ic_play)
+            }
+            else if (timerState ==ChronometerState.Paused){
+                timerState = ChronometerState.Running
+                binding?.totalChronometre?.base= SystemClock.elapsedRealtime() - elapse
+                binding?.totalChronometre?.start()
+               //binding?.currentInterval?.start()
+                binding?.fabStartPause?.setImageResource(R.drawable.ic_pause)
+            }
+            _binding?.fabIntervalClear?.setOnClickListener { }
+        }
+    }
+
+    // TODO -- implement recyclrview with result
+    // TODO -- init all functionality
 }
